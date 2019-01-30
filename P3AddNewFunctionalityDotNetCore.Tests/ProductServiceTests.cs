b3a940165404eff90806fbb0d0295fc9bf4e9411
davidpmpbennett <47103327+davidpmpbennett@users.noreply.github.com>
2019-01-30 08:49:29 +0700

    Add files via upload

---
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public class ProductServiceTests
+    {
+        /// <summary>
+        /// Take this test method as a template to write your test method.
+        /// A test method must check if a definite method does its job:
+        /// returns an expected value from a particular set of parameters
+        /// </summary>
+        [Fact]
+        public void ExampleMethod()
+        {
+            // Arrange
+
+            // Act
+
+
+            // Assert
+            Assert.Equal(1, 1);
+        }
+
+        // TODO write test methods to ensure a correct coverage of all possibilities
+    }
+}
