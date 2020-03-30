2e9ca1b9d1e31aef9c27744f7b7eccc3e8a7fbdf
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-30 14:22:30 +1100

    Completed unit tests for ProductRepository

--- P3AddNewFunctionalityDotNetCore.Tests/RepositoryTests/ProductRepositoryTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/RepositoryTests/ProductRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using MockQueryable.Moq;
 using Moq;
 using P3AddNewFunctionalityDotNetCore.Data;
@@ -23,6 +24,9 @@ namespace P3AddNewFunctionalityDotNetCore.UnitTests.RepositoryTests
             _mockContext = new Mock<P3Referential>();
 
             _mockDbSetProducts = GetMockProducts().AsQueryable().BuildMockDbSet();
+
+            _mockDbSetProducts.Setup(x => x.Remove(It.IsAny<Product>())).Returns(It.IsAny<EntityEntry<Product>>());
+            _mockDbSetProducts.Setup(x => x.Add(It.IsAny<Product>())).Returns(It.IsAny<EntityEntry<Product>>());
 
             _mockContext.SetupGet(x => x.Product).Returns(_mockDbSetProducts.Object);
         }
@@ -57,6 +61,15 @@ namespace P3AddNewFunctionalityDotNetCore.UnitTests.RepositoryTests
                 Price = 10.30,
                 Quantity = 40
             });
+            products.Add(new Product()
+            {
+                Id = 4,
+                Description = "product 3 desc",
+                Details = "product 3 details",
+                Name = "product 3 name",
+                Price = 10.30,
+                Quantity = 0
+            });
             return products;
         }
 
@@ -77,5 +90,135 @@ namespace P3AddNewFunctionalityDotNetCore.UnitTests.RepositoryTests
             Assert.Equal(expectedProduct, actualProduct, new ProductEqualityComparator());
         }
 
+
+        [Fact]
+        public void GetProduct_WithoutParams_ShouldReturnCorrectValue()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            var result = sut.GetProduct();
+
+            //Assert
+            var taskProducts = Assert.IsType<Task<IList<Product>>>(result);
+            var products = taskProducts.Result;
+            Assert.Equal(GetMockProducts().ToList<Product>().Count, products.Count);
+        }
+
+        [Fact]
+        public void GetAllProducts_ShouldReturnCorrectValue()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            var result = sut.GetAllProducts();
+
+            //Assert
+            var products = Assert.IsAssignableFrom<IEnumerable<Product>>(result);
+            var productsList = products as List<Product>;
+            Assert.Equal(GetMockProducts().ToList<Product>().Count, productsList.Count);
+        }
+
+        [Fact]
+        public void UpdateProductStocks_ProductStock_LessThanZero_ShouldCallCorrectMethod()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            sut.UpdateProductStocks(4, 10);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Remove(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void UpdateProductStocks_ProductStock_EqualToZero_ShouldCallCorrectMethod()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            sut.UpdateProductStocks(4, 0);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Remove(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void UpdateProductStocks_ProductStock_GreaterThanZero_ShouldCallCorrectMethod()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            sut.UpdateProductStocks(1, 10);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Update(It.IsAny<Product>()), Times.Once);
+            _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Fact]
+        public void SaveProduct_ProductIsNull_ShouldNotCallMethods()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+            Product product = null;
+
+            //Act
+            sut.SaveProduct(product);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Add(It.IsAny<Product>()), Times.Never);
+            _mockContext.Verify(x => x.SaveChanges(), Times.Never);
+        }
+
+        [Fact]
+        public void SaveProduct_ProductIsNotNull_ShouldNotCallMethods()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+            Product product = GetMockProducts().ToList<Product>().Find(x => x.Id == 1);
+            
+            //Act
+            sut.SaveProduct(product);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Add(It.IsAny<Product>()), Times.Once);
+            _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteProduct_ProductIsNull_ShouldNotCallMethods()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            var productId = 10; //pass product Id that doesn't exist in mock dataset
+            sut.DeleteProduct(productId);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Remove(It.IsAny<Product>()), Times.Never);
+            _mockContext.Verify(x => x.SaveChanges(), Times.Never);
+        }
+
+        [Fact]
+        public void DeleteProduct_ProductIsNotNull_ShouldNotCallMethods()
+        {
+            //Arrange
+            var sut = new ProductRepository(_mockContext.Object);
+
+            //Act
+            var productId = 1; //pass product Id that exists in mock dataset
+            sut.DeleteProduct(productId);
+
+            //Assert
+            _mockDbSetProducts.Verify(x => x.Remove(It.IsAny<Product>()), Times.Once);
+            _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
     }
 }
