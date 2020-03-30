a3e823bc2366577283de2f9f130206e196487c04
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-30 16:48:39 +1100

    Added Order comparator

---
+++ P3AddNewFunctionalityDotNetCore.Tests/Comparators/OrderEqualityComparator.cs
@@ -0,0 +1,33 @@
+﻿using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3AddNewFunctionalityDotNetCore.UnitTests.Comparators
+{
+    class OrderEqualityComparator : IEqualityComparer<Order>
+    {
+        public bool Equals(Order x, Order y)
+        {
+            if (x.Id == y.Id &&
+               x.Address == y.Address &&
+               x.City == y.City &&
+               x.Country == y.Country &&
+               x.Name == y.Name &&
+               x.Zip == y.Zip)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode(Order obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
