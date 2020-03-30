ef06a6f686f63fa42c59e0d154d54aec4d98cd0d
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-30 19:33:07 +1100

    Added project reference and some refactoring

--- IntegrationTests/ServiceIntegrationTests/ProductServiceIntegrationTests.cs
+++ IntegrationTests/ServiceIntegrationTests/ProductServiceIntegrationTests.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace IntegrationTests
+namespace P3AddNewFunctionalityDotNetCore.IntegrationTests.ServiceIntegrationTests
 {
     public class ProductServiceIntegrationTests
     {
